32fbe62c1faa620ff4e2a4e77d25f6004f1fe190
Руднев Геннадий Владимирович <grudnev@systematica-consulting.ru>
2019-08-29 19:33:55 +0300

    Initial commit

---
+++ src/main/java/com/example/rpws/chapters/SpringBootAesome/SpringBootAesomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpws.chapters.SpringBootAesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAesomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAesomeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/rpws/chapters/SpringBootAesome/model/Temperature.java
@@ -0,0 +1,14 @@
+package com.example.rpws.chapters.SpringBootAesome.model;
+
+public class Temperature {
+    private final double value;
+
+    public Temperature(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
+
